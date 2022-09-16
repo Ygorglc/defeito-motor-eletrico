c9487f88f4e356523897090a57744cfed0abed13
ygorglc <ygorglc@gmail.com>
2022-09-16 12:35:17 -0300

    feature: fazendo o websocket aceitar todos as origens

--- src/main/java/com/costa/ygor/defeito_motor_eletrico/GreetingController.java
+++ src/main/java/com/costa/ygor/defeito_motor_eletrico/GreetingController.java
@@ -3,6 +3,7 @@ package com.costa.ygor.defeito_motor_eletrico;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.HtmlUtils;
@@ -11,6 +12,7 @@ import org.springframework.web.util.HtmlUtils;
 public class GreetingController {
 
     @MessageMapping("/hello")
+    @CrossOrigin("*")
     @SendTo("/defeito-motor-eletrico/topic/greetings")
     public Greeting greet(HelloMessage message) throws InterruptedException {
         Thread.sleep(2000);
